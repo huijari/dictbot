cc791f0efad586074286f48db461002ce4b8a743
Alexandre Cesar <acmorcego@gmail.com>
2017-12-14 16:41:13 -0200

    Setup the bot dispatch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 const { createStore } = require('redux')
+const { fetch } = require('node-fetch')
 const { json } = require('micro')
 const { Map } = require('immutable')
 
 const handle = require('./handler')
 const reducer = require('./reducer')
+const { dispatch } = require('./bot')
 
 const store = createStore(reducer)
 const handler = handle(store)
+const dispatchCommand = dispatch({
+  fetch,
+  token: process.ENV['BOT_TOKEN']
+})
 
 const app = async (req, res) => {
   const update = await json(req)
   const result = handler(update.message)
   if (result.action) store.dispatch(result.action)
+  if (result.command) dispatchCommand(result.command)
   return result
 }
 
