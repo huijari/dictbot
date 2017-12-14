76dc15fa89bccc547af069bd741aa78222fbd8a2
Alexandre Cesar <acmorcego@gmail.com>
2017-12-14 15:59:54 -0200

    Use int ids instead of strings

--- src/handler/bang.js
+++ src/handler/bang.js
@@ -5,14 +5,14 @@ const handler = ({ getState }) => ({ text, from, chat }) => {
 
   const key = text.substring(1)
 
-  const chatMessage = getState().getIn(['chat', `${chat.id}`, key])
+  const chatMessage = getState().chat.getIn([chat.id, key])
 
   if (chatMessage) {
     const command = bot.send(chat.id, chatMessage)
     return { command }
   }
 
-  const userMessage = getState().getIn(['user', `${from.id}`, key])
+  const userMessage = getState().user.getIn([from.id, key])
 
   if (userMessage) {
     const command = bot.send(chat.id, userMessage)
