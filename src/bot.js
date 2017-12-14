767da0ef1c0167a5bd99bebad8cf4b749fe94c0c
Alexandre Cesar <acmorcego@gmail.com>
2017-12-14 16:31:52 -0200

    Implement dispatch for bot commands

--- src/bot.js
+++ src/bot.js
@@ -13,9 +13,30 @@ const reply = (chat, message, text) => ({
   text
 })
 
+const request = ({ fetch, token }, method, params) =>
+  fetch(`api.telegram.org/bot${token}/${method}?${params}`)
+const requestSendd = (conf, { chat, text }) =>
+  request(conf, 'sendMessage', `chat_id=${chat}&text=${text}`)
+const requestReply = (conf, { chat, message, text }) =>
+  request(
+    conf,
+    'sendMessage',
+    `chat_id=${chat}&text=${text}&reply_to_message_id=${message}`
+  )
+
+const dispatch = conf => command => {
+  switch (command.type) {
+    case SEND:
+      return requestSend(conf, command)
+    case REPLY:
+      return requestReply(conf, command)
+  }
+}
+
 module.exports = {
   SEND,
   send,
   REPLY,
-  reply
+  reply,
+  dispatch
 }
